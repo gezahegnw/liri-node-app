d24592fac6ebe112c1198b2a320c6ebbc7cc7fbe
gezahegnw <gezahegnw@gmail.com>
2018-10-20 22:03:03 -0500

    last updated

--- keys.js
+++ keys.js
@@ -11,12 +11,7 @@ const spotifyKeys = {
     client_secret: "dc4b24ef911941aca309de6a30db799c"
     
   }
-  // OMDB api key
-//const omdbKey = "3bc50c76";
-//bandsintwon api key
-//const bandsInTownKey= bd080d4d6b0498546f5f5eb209ca0575;
+  
 
 
 module.exports.spotifyKeys = spotifyKeys;
-//module.exports.omdbKey = omdbKey;
-//module.exports.bandsInTownKey = bandsInTownKey;
